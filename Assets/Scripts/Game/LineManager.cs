3dc7f64af24a537791614258abc0df043597ad3a
Estelar <localhost>
2026-06-17 15:17:56 +0500

    create level manager

---
+++ Assets/Scripts/Game/LineManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Zenject;
+
+namespace Game
+{
+    public class LineManager : MonoBehaviour
+    {
+        public int step = 2; // Расстояние между линиями
+        public float radius = 2; // Расстояние от центра до сущности на линии
+        public float height = 2; // Высота линии
+        public Vector3 startPoint = Vector3.zero;
+        public GameObject prefab;
+        private Queue<Line> _lines = new Queue<Line>();
+
+        public void CreateLine()
+        {
+            if (prefab == null)
+                throw new ArgumentException("prefab is null");
+
+            Vector3 left = startPoint;
+            Vector3 right = startPoint;
+            left.y += height;
+            right.y += height;
+            left.x -= radius;
+            right.x += radius;
+            _lines.Enqueue(new Line(
+                Instantiate(prefab, left, Quaternion.identity),
+                Instantiate(prefab, right, Quaternion.identity)
+            ));
+        }
+
+        public void StepDown()
+        {
+            foreach (var item in _lines)
+            {
+                item.left.transform.position += Vector3.down * step;
+                item.right.transform.position += Vector3.down * step;
+            }
+        }
+
+        public void DestroyFirstLine()
+        {
+            Line line = _lines.Dequeue();
+            Destroy(line.left);
+            Destroy(line.right);
+        }
+
+        public Line Peek()
+            => _lines.Peek();
+    }
+
+    public struct Line
+    {
+        public GameObject left, right;
+
+        public Line(GameObject left, GameObject right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
